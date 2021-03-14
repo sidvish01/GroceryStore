1ce79496092645830a8839be289a4d13f55701a3
sidvish01 <siddartha.vishnek@gmail.com>
2021-03-15 03:10:26 +0530

    session storage cart

--- cart.js
+++ cart.js
@@ -1,8 +1,49 @@
+//checkEmptyCart()
+var products = [
+  {
+    name: "Hellman's Mayonnaise",
+    price: "$19.49",
+    img: "mayonnaise.png",
+  },
+  {
+    name: "Diced Tomatoes",
+    price: "$12.99",
+    img: "dtomatoes.png",
+  },
+];
+
+//var cartRow = document.createElement('tr')
+
+var cart = document.getElementsByClassName("try")[0];
+var cartRow = document.createElement('div')
+var cartRowContent = `
+<table class="cart">
+<tr class="cart-fields">
+    <th>Item</th>
+    <th>Quantity</th>
+    <th>Price</th>
+</tr>
+<tr class="cart-item">
+    <td class="item-name"><img src="${products[0].img}" alt="Mayonnaise Container" width="150"
+            height="150"><br>Hellman's
+        Mayonnaise
+    </td>
+    <td>
+        <button class="button-cart-remove" type="button">-</button> <span class="item-quantity">${sessionStorage.getItem(products[0].name)}</span>
+        <button class="button-cart-add" type="button">+</button>
+    </td>
+    <td class="item-price">${products[0].price}</td>
+</tr></table>`;
+cartRow.innerHTML = cartRowContent
+cart.prepend(cartRow)
+
+
 var removeQuantityButton = document.getElementsByClassName(
   "button-cart-remove"
 );
 var addQuantityButton = document.getElementsByClassName("button-cart-add");
 var itemQuantity = document.getElementsByClassName("item-quantity");
+updateCartTotal()
 
 //console.log(document.getElementsByClassName("item-quantity")[0].innerHTML)
 for (var i = 0; i < removeQuantityButton.length; i++) {
@@ -15,6 +56,7 @@ for (var i = 0; i < removeQuantityButton.length; i++) {
         var buttonClicked = event.target;
         buttonClicked.parentNode.parentNode.remove();
         updateCartTotal();
+        sessionStorage.setItem("Hellman's Mayonnaise", 0);
         checkEmptyCart();
       }
     });
@@ -28,11 +70,13 @@ for (var i = 0; i < removeQuantityButton.length; i++) {
         if (removePrompt == true) {
           buttonClicked.parentNode.remove();
           updateCartTotal();
+          sessionStorage.setItem("Hellman's Mayonnaise", 0);
           checkEmptyCart();
         }
       } else {
         buttonClicked.children[1].innerHTML -= 1;
         updateCartTotal();
+        sessionStorage.setItem("Hellman's Mayonnaise", buttonClicked.children[1].innerHTML);
         checkEmptyCart();
       }
     });
@@ -46,6 +90,7 @@ for (var i = 0; i < addQuantityButton.length; i++) {
     buttonClicked.children[1].innerHTML =
       parseInt(buttonClicked.children[1].innerHTML) + 1;
     updateCartTotal();
+    sessionStorage.setItem("Hellman's Mayonnaise", buttonClicked.children[1].innerHTML);
     checkEmptyCart();
   });
 }
@@ -88,15 +133,14 @@ function checkEmptyCart() {
   }
 }
 
-//var addtocartbutton = document.getElementById('add-to-cart')
-//addtocartbutton.addEventListener("click", function (event) {
-//var buttonClicked = event.target
 function addtocartbutton() {
+  var key = document.getElementById("name").innerHTML;
   if (document.getElementById("cart-tab").innerHTML == "Shopping Cart") {
     var addItems = parseInt(document.getElementById("qty").innerHTML);
     document.getElementById("cart-tab").innerHTML =
       "Shopping Cart (" + addItems + ")";
     document.getElementById("qty").innerHTML = 1;
+    addToSessionStorage(key, addItems);
   } else {
     if (document.getElementById("cart-tab").innerHTML.length == 17) {
       var noOfItems = parseInt(
@@ -106,6 +150,8 @@ function addtocartbutton() {
       document.getElementById("cart-tab").innerHTML =
         "Shopping Cart (" + (noOfItems + addItems) + ")";
       document.getElementById("qty").innerHTML = 1;
+      addToSessionStorage(key, noOfItems + addItems);
+      console.log(sessionStorage);
     } else if (document.getElementById("cart-tab").innerHTML.length == 18) {
       var noOfItems = parseInt(
         document.getElementById("cart-tab").innerHTML.substring(15, 17)
@@ -114,6 +160,8 @@ function addtocartbutton() {
       document.getElementById("cart-tab").innerHTML =
         "Shopping Cart (" + (noOfItems + addItems) + ")";
       document.getElementById("qty").innerHTML = 1;
+      addToSessionStorage(key, noOfItems + addItems);
+      console.log(sessionStorage);
     } else {
       var noOfItems = parseInt(
         document.getElementById("cart-tab").innerHTML.substring(15, 18)
@@ -122,6 +170,8 @@ function addtocartbutton() {
       document.getElementById("cart-tab").innerHTML =
         "Shopping Cart (" + (noOfItems + addItems) + ")";
       document.getElementById("qty").innerHTML = 1;
+      addToSessionStorage(key, noOfItems + addItems);
+      console.log(sessionStorage);
     }
   }
 }
@@ -131,9 +181,45 @@ function increaseQuantity() {
     parseInt(document.getElementById("qty").innerHTML) + 1;
 }
 function decreaseQuantity() {
-  document.getElementById("qty").innerHTML =
-    parseInt(document.getElementById("qty").innerHTML) - 1;
+  if (parseInt(document.getElementById("qty").innerHTML) > 1)
+    document.getElementById("qty").innerHTML =
+      parseInt(document.getElementById("qty").innerHTML) - 1;
 }
 
-console.log(document.getElementById("cart-tab").innerHTML.length);
+function moreDescription() {
+  if (
+    document.getElementById("more-info").children[0].innerHTML ==
+    "More Description"
+  ) {
+    document.getElementById("dots").style.display = "none";
+    document.getElementById("more").style.display = "inline";
+    document.getElementById("more-info").children[0].innerHTML =
+      "Less Description";
+  } else {
+    document.getElementById("dots").style.display = "inline";
+    document.getElementById("more").style.display = "none";
+    document.getElementById("more-info").children[0].innerHTML =
+      "More Description";
+  }
+}
+
+function addToSessionStorage(itemName, itemQuantity) {
+  sessionStorage.setItem(itemName, itemQuantity);
+}
+
 //})
+
+for (var i = 0; i<sessionStorage.length-1; i++){
+    var slimContent = slimContent+`<tr class="cart-item">
+    <td class="item-name"><img src="${products[i].img}" alt="Mayonnaise Container" width="150"
+            height="150"><br>Hellman's
+        Mayonnaise
+    </td>
+    <td>
+        <button class="button-cart-remove" type="button">-</button> <span class="item-quantity">${sessionStorage.getItem(products[i].name)}</span>
+        <button class="button-cart-add" type="button">+</button>
+    </td>
+    <td class="item-price">${products[i].price}</td>
+</tr>`
+}
+console.log(slimContent)
