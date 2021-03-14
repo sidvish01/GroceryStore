461b49b6d015e4882254b5791398860ae3e084df
sidvish01 <siddartha.vishnek@gmail.com>
2021-03-14 17:09:32 +0530

    fixed empty cart

--- cart.js
+++ cart.js
@@ -1,59 +1,86 @@
-
-var removeQuantityButton = document.getElementsByClassName('button-cart-remove')
-var addQuantityButton = document.getElementsByClassName('button-cart-add')
-var itemQuantity = document.getElementsByClassName('item-quantity')
+var removeQuantityButton = document.getElementsByClassName("button-cart-remove");
+var addQuantityButton = document.getElementsByClassName("button-cart-add");
+var itemQuantity = document.getElementsByClassName("item-quantity");
 
 //console.log(document.getElementsByClassName("item-quantity")[0].innerHTML)
 for (var i = 0; i < removeQuantityButton.length; i++) {
-    var button = removeQuantityButton[i]
-    if (itemQuantity[i].innerHTML == 1) {
-        button.addEventListener('click', function (event) {
-            var buttonClicked = event.target
-            buttonClicked.parentNode.parentNode.remove()
-            updateCartTotal()
-        })
-    }
-    else {
-        button.addEventListener('click', function (event) {
-            var buttonClicked = (event.target).parentNode
-            if (buttonClicked.children[1].innerHTML == 1) {
-                buttonClicked.parentNode.remove()
-                updateCartTotal()
-            }
-            else {
-                buttonClicked.children[1].innerHTML -= 1
-                updateCartTotal()
-            }
-        })
-    }
-
+  var button = removeQuantityButton[i];
+  if (itemQuantity[i].innerHTML == 1) {
+    console.log(itemQuantity[i].innerHTML);
+    button.addEventListener("click", function (event) {
+      var removePrompt = confirm("Do you want to remove this item from cart?");
+      if (removePrompt == true) {
+        var buttonClicked = event.target;
+        buttonClicked.parentNode.parentNode.remove();
+        updateCartTotal();
+        checkEmptyCart()
+      }
+    });
+  } else {
+    button.addEventListener("click", function (event) {
+      var buttonClicked = event.target.parentNode;
+      if (buttonClicked.children[1].innerHTML == 1) {
+        var removePrompt = confirm(
+          "Do you want to remove this item from cart?"
+        );
+        if (removePrompt == true) {
+          buttonClicked.parentNode.remove();
+          updateCartTotal();
+          checkEmptyCart()
+        }
+      } else {
+        buttonClicked.children[1].innerHTML -= 1;
+        updateCartTotal();
+        checkEmptyCart()
+      }
+    });
+  }
 }
 
 for (var i = 0; i < addQuantityButton.length; i++) {
-    var button = addQuantityButton[i]
-    button.addEventListener('click', function (event) {
-        var buttonClicked = (event.target).parentNode
-        buttonClicked.children[1].innerHTML = parseInt(buttonClicked.children[1].innerHTML) + 1
-        updateCartTotal()
-    })
+  var button = addQuantityButton[i];
+  button.addEventListener("click", function (event) {
+    var buttonClicked = event.target.parentNode;
+    buttonClicked.children[1].innerHTML =
+      parseInt(buttonClicked.children[1].innerHTML) + 1;
+    updateCartTotal();
+    checkEmptyCart()
+  });
 }
-
 
 function updateCartTotal() {
-    var cartItem = document.getElementsByClassName('cart-item')
-    var total = 0
-    for (var i = 0; i < cartItem.length; i++) {
-        var price = parseFloat(cartItem[i].getElementsByClassName('item-price')[0].innerText.replace('$', ''))
-        var quantity = parseInt(cartItem[i].getElementsByClassName('item-quantity')[0].innerText)
-        total = total + (price * quantity)
-        //console.log(price, quantity)
-    }
-    document.getElementsByClassName('subtotal')[0].innerText = total.toFixed(2)
-    document.getElementsByClassName('qst')[0].innerText = (.09975 * total).toFixed(2)
-    document.getElementsByClassName('gst')[0].innerText = (0.05 * total).toFixed(2)
-    document.getElementsByClassName('total')[0].innerText = (parseFloat(document.getElementsByClassName('subtotal')[0].innerText) +
-        parseFloat(document.getElementsByClassName('qst')[0].innerText) + parseFloat(document.getElementsByClassName('gst')[0].innerText)).toFixed(2)
-
+  var cartItem = document.getElementsByClassName("cart-item");
+  var total = 0;
+  for (var i = 0; i < cartItem.length; i++) {
+    var price = parseFloat(
+      cartItem[i]
+        .getElementsByClassName("item-price")[0]
+        .innerText.replace("$", "")
+    );
+    var quantity = parseInt(
+      cartItem[i].getElementsByClassName("item-quantity")[0].innerText
+    );
+    total = total + price * quantity;
+  }
+  document.getElementsByClassName("subtotal")[0].innerText = total.toFixed(2);
+  document.getElementsByClassName("qst")[0].innerText = (
+    0.09975 * total
+  ).toFixed(2);
+  document.getElementsByClassName("gst")[0].innerText = (0.05 * total).toFixed(
+    2
+  );
+  document.getElementsByClassName("total")[0].innerText = (
+    parseFloat(document.getElementsByClassName("subtotal")[0].innerText) +
+    parseFloat(document.getElementsByClassName("qst")[0].innerText) +
+    parseFloat(document.getElementsByClassName("gst")[0].innerText)
+  ).toFixed(2);
 }
-console.log(document.getElementsByClassName('subtotal')[0].innerText)
 
+function checkEmptyCart() {
+    var cartItem = document.getElementsByClassName("cart-item")
+    if (cartItem.length==0){
+        alert("Your cart is empty, let's take you to homepage and add some items!!")
+        location.replace("index.html")
+
+    }
+}
