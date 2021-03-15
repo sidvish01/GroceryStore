e30b835e9dde77aee37b9c55eb11f80edb00967a
sidvish01 <siddartha.vishnek@gmail.com>
2021-03-16 01:04:50 +0530

    aisle fix

--- oatsAndDairyP2.js
+++ oatsAndDairyP2.js
@@ -1,7 +1,15 @@
-function SaveProduct(key){
-
-    var name = key;
-
-    sessionStorage.setItem(name, 1);
-
+function SaveProduct(name){
+    var itemName = ""+name
+    console.log(itemName)
+    addToSessionStorage(name, 1)
 }
+
+function addToSessionStorage(itemName, itemQuantity) {
+    if (sessionStorage.getItem(itemName) != null) {
+      sessionStorage.setItem(itemName, (parseInt(sessionStorage.getItem(itemName)) + parseInt(itemQuantity)));
+      
+    } else {
+      sessionStorage.setItem(itemName, itemQuantity);
+    }
+    //showShowCartTab();
+  }
