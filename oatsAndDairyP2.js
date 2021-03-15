c5e83e6a8f0b9860d1d9a22a56a18af12b342e76
sidvish01 <siddartha.vishnek@gmail.com>
2021-03-16 01:51:13 +0530

    ali

--- oatsAndDairyP2.js
+++ oatsAndDairyP2.js
@@ -1,15 +1,7 @@
-function SaveProduct(name){
-    var itemName = ""+name
-    console.log(itemName)
-    addToSessionStorage(name, 1)
-}
+function SaveProduct(id){
 
-function addToSessionStorage(itemName, itemQuantity) {
-    if (sessionStorage.getItem(itemName) != null) {
-      sessionStorage.setItem(itemName, (parseInt(sessionStorage.getItem(itemName)) + parseInt(itemQuantity)));
-      
-    } else {
-      sessionStorage.setItem(itemName, itemQuantity);
-    }
-    //showShowCartTab();
-  }
+  var name = id;
+
+  sessionStorage.setItem( name,1);
+
+}
