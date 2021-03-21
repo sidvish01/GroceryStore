ed0fcd16a8409d1b2aeb19dcf63c389fe308e7b3
sidvish01 <siddartha.vishnek@gmail.com>
2021-03-21 22:08:55 +0530

    ali aisle button edit

--- oatsAndDairyP2.js
+++ oatsAndDairyP2.js
@@ -2,6 +2,12 @@ function SaveProduct(id){
 
   var name = id;
 
-  sessionStorage.setItem( name,1);
+  //sessionStorage.setItem( name,1);
+  if (sessionStorage.getItem(name) != null) {
+      sessionStorage.setItem(name, (parseInt(sessionStorage.getItem(name)) + parseInt(1)));
+      
+    } else {
+      sessionStorage.setItem(name, 1);
+    }
 
 }
