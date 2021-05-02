6f5ce2ec20a9b13fac20cec1fde7b52a68186755
sidvish01 <siddartha.vishnek@gmail.com>
2021-05-02 13:45:51 -0400

    sadsad

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
-//header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-//$xml = file_get_contents('php://input');
-//$fh = fopen('orderinfo.xml', 'a');
-//fwrite($fh, $xml);
-//fclose($fh);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+$xml = file_get_contents('php://input');
+$fh = fopen('orderinfo.xml', 'a');
+fwrite($fh, $xml);
+fclose($fh);
 
 ?>
